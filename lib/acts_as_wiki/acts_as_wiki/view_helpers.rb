233e14015f66218ac27d25644d396813a4e5aaa8
kris fox <kris.fox@gmail.com>
2011-02-02 17:59:08 -0800

    added some support for formfor stuff

--- lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
+++ lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
@@ -1,5 +1,5 @@
 module ActsAsWiki::Markable
-	module ViewHelpers
+	module FormTagHelper
 		def wiki_text_field_tag(name, value = nil, options = {})
 			text_field_tag(name, value, options)
 		end
@@ -9,7 +9,27 @@ module ActsAsWiki::Markable
 		end
 	end
 	
-	module FormTagHelpers
+	module FormBuilder
+		def wiki_text_field(method, options = {})
+			@template.send(
+				"wiki_text_field",
+				@object_name,
+				method,
+				objectify_options(options)
+			)
+		end
+		def wiki_text_area(method, options = {})
+			@template.send(
+				"wiki_text_area",
+				@object_name,
+				method,
+				objectify_options(options)
+			)
+		end
+	end
+	
+	module FormHelper
+		
 		def wiki_text_field(object_name, method, options = {})
 			if @object.responds_to?(:has_markup?)
 				if @object.has_markup?
