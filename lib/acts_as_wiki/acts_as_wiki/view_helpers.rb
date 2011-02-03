240a07f239bc7a74eeb12915bf37e7fecb77f3c7
kris fox <kris.fox@gmail.com>
2011-02-03 15:59:58 -0800

    fixed issue with text field override not working

--- lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
+++ lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
@@ -9,26 +9,23 @@ module ActsAsWiki::Markable
 		end
 		
 		def text_field_with_wiki(method, options = {})
-			if @object.respond_to?(:has_markup?) && @object.has_markup? && method.to_sym == @object.wiki_text_column.to_sym
+			if @object.respond_to?(:has_markup?) && @object.has_markup? && (method.to_s == @object.class.acts_as_wiki_options[:column])
 				self.fields_for :wiki_markup do |w_markup|
 					w_markup.text_field :markup, options
 				end
 			else
-				# @template.text_field @object_name, method, objectify_options(options)
 				text_field_without_wiki method, options
 			end
 		end
 		
 		def text_area_with_wiki(method, options = {})
-			if @object.respond_to?(:has_markup?) && @object.has_markup? && method.to_sym == @object.wiki_text_column.to_sym
+			if @object.respond_to?(:has_markup?) && @object.has_markup? && (method.to_s == @object.class.acts_as_wiki_options[:column])
 				self.fields_for :wiki_markup do |w_markup|
 					w_markup.text_area :markup, options
 				end
 			else
-				# @template.text_area @object_name, method, objectify_options(options)
 				text_area_without_wiki method, options
 			end
 		end
-		
 	end
 end
