37b3c6ea8420eb956ed621a1a8b7aabdf2d92116
kris fox <kris.fox@gmail.com>
2011-02-02 17:39:04 -0800

    added text area to helper

--- lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
+++ lib/acts_as_wiki/acts_as_wiki/view_helpers.rb
@@ -3,6 +3,10 @@ module ActsAsWiki::Markable
 		def wiki_text_field_tag(name, value = nil, options = {})
 			text_field_tag(name, value, options)
 		end
+		
+		def wiki_text_area(name, value = nil, options = {})
+			text_field_tag(name, value, options)
+		end
 	end
 	
 	module FormTagHelpers
@@ -17,6 +21,20 @@ module ActsAsWiki::Markable
 				end
 			else
 				text_field object_name, method, options
+			end
+		end
+		
+		def wiki_text_area(object_name, method, options = {})
+			if @object.responds_to?(:has_markup?)
+				if @object.has_markup?
+					fields_for :wiki_markup do |w_markup|
+						w_markup.text_area :markup, options
+					end
+				else
+					text_area object_name, method, options
+				end
+			else
+				text_area object_name, method, options
 			end
 		end
 	end
